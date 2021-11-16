2a235b1e1fbde7f5d910c080770d6bb8b1d61b7a
sheiler <silvio.heiler@gmail.com>
2021-11-16 18:16:44 -0300

    2. Generar entidades
    Del documento funcional vemos las entidades que están definidas para intercambio con el servicio más las necesarias para operar en todas las capas (TarjetaCredito, TransactionResult, Enums, Exception, y una adicional OperadorFormulario que vamos a usar para hacer operaciones del formulario necesarias para el punto E y F)

---
+++ Banco/Banco.Entidades/Dominio/TarjetaCredito.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco.Entidades.Dominio
+{
+    [DataContract]
+    public class TarjetaCredito
+    {
+        private int _id;
+        private int _tipo;
+        private int _periodo;
+        private string _nroPlastico;
+        private string _usuario;
+        private int _idCliente;
+        private double _limiteCompra;
+        private Cliente _cliente;
+
+        public int Id { get => _id; set => _id = value; }
+
+        [DataMember]
+        public int Tipo { get => _tipo; set => _tipo = value; }
+        
+        [DataMember(Name = "PeriodoVencimiento")]
+        public int Periodo { get => _periodo; set => _periodo = value; }
+        [DataMember]
+        public string NroPlastico { get => _nroPlastico; set => _nroPlastico = value; }
+        [DataMember]
+        public string Usuario { get => _usuario; set => _usuario = value; }
+        [DataMember]
+        public int IdCliente { get => _idCliente; set => _idCliente = value; }
+        [DataMember]
+        public double LimiteCompra { get => _limiteCompra; set => _limiteCompra = value; }
+        public Cliente Cliente { get => _cliente; set => _cliente = value; }
+
+        public TarjetaCredito(int tipo, int periodo, string plastico, string usuario, int idCliente)
+        {
+            this._tipo = tipo;
+            this._periodo = periodo;
+            this._nroPlastico = plastico;
+            this._usuario = usuario;
+            this._idCliente = idCliente;
+        }
+
+    }
+}
