d71c8f13810ee08c645316a31fe1760a391eff47
sheiler <silvio.heiler@gmail.com>
2021-11-16 14:24:52 -0300

    1. Generar formulario
    Se genera el formulario con los controles necesarios copiando la pantalla que se da en el enunciado del problema.
    Además se planifican los eventos que van a ser necesarios: formulario (onload), calcular (click), alta (click).

---
+++ Banco/Banco.UIForms/FrmTarjetaCredito.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banco.UIForms
+{
+    public partial class FrmTarjetaCredito : Form
+    {
+        public FrmTarjetaCredito()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmTarjetaCredito_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void _btnCalcular_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void _btnAlta_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
